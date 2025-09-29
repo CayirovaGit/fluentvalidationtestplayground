969805318f0bba9d8e9e10ae8f55a5148a013510
Turkmen48 <enesusta6638@gmail.com>
2025-09-29 21:11:14 +0300

    initial

--- fluentvalidationtestplayground/Program.cs
+++ fluentvalidationtestplayground/Program.cs
@@ -4,7 +4,6 @@ using fluentvalidationtestplayground.Validators.Customer;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddValidatorsFromAssemblyContaining<CreateCustomerValidator>().AddControllersWithViews()
     .AddViewLocalization();
 
